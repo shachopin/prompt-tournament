1eba7773b40ab52cf3fab7c5f85932f447c3115f
shachopin <shachopin@gmail.com>
2025-09-03 03:24:01 +0000

    change for codespace

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   experimental: {
     dynamicIO: true,
+    serverActions: { //needed for github codespaces only, not anywhere else
+      allowedOrigins: ['localhost:3000'],
+    },
   },
   typescript: {
     ignoreBuildErrors: true,
